d0b25267c81f24f72f8b7c47bb4c2f6cc0e78f26
Jason Clarke <jclarke@student.neumont.edu>
2020-02-12 17:54:04 -0700

    Created the Project

---
+++ Mysterious-Insiders/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mysterious_Insiders.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
