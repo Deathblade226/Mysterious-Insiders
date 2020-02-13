4291b011595c641c5a91f78774fd8ecb16799df3
EternityDoom <EternityDoom@users.noreply.github.com>
2020-02-12 18:52:27 -0700

    Added the ModuleData class. This is my first commit, and will test to make sure I can commit with no problems.

---
+++ Mysterious-Insiders/Models/ModuleData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class ModuleData
+{
+    public enum moduleType
+    {
+        NONE,
+        CHECK,
+        TEXT,
+        MENU,
+        NUMERIC,
+        DERIVATIVE
+    }
+
+    public moduleType ModuleType { get; set; } = moduleType.NONE;
+
+    public ModuleData()
+    {
+
+    }
+}
