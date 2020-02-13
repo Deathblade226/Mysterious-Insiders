bb1fd9680e5aff702a01d06f6546f75ffab1af18
EternityDoom <EternityDoom@users.noreply.github.com>
2020-02-12 19:38:50 -0700

    Added more properties to ModuleData.

--- Mysterious-Insiders/Models/ModuleData.cs
+++ Mysterious-Insiders/Models/ModuleData.cs
@@ -9,13 +9,25 @@ public class ModuleData
         TEXT,
         MENU,
         NUMERIC,
-        DERIVATIVE
+        DERIVATIVE,
+        ROLL
     }
 
-    public moduleType ModuleType { get; set; } = moduleType.NONE;
+    private moduleType m_moduleType;
 
-    public ModuleData()
+    public moduleType ModuleType { get { return m_moduleType; } }
+    public string Id { get; set; }
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public int BgImageIndex { get; set; }
+    public Color TextColor { get; set; }
+
+    protected ModuleData(moduleType ModuleType)
     {
-
+        m_moduleType = ModuleType;
     }
+
+    public ModuleData() : this(moduleType.NONE) {}
 }
