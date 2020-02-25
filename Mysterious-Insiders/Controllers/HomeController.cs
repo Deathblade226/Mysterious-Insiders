d5f0d6d2c903c9ab6532bb724dc5535f0ab0ba31
Topanga <tpatterson@student.neumont.edu>
2020-02-24 19:17:56 -0700

    Style and Comment Code
    Added more basic styling and added a piece of commented code to the home controller

--- Mysterious-Insiders/Controllers/HomeController.cs
+++ Mysterious-Insiders/Controllers/HomeController.cs
@@ -61,5 +61,18 @@ namespace Mysterious_Insiders.Controllers
         {
             return View();
         }
+
+        //This is testing code for embedding a pdf file into the cshtml
+        [HttpPost]
+        public ActionResult ViewPDF()
+        {
+            string embed = "<object data=\"{0}\" type=\"application/pdf\" width=\"500px\" height=\"300px\">";
+            embed += "If you are unable to view file, you can download from <a href = \"{0}\">here</a>";
+            embed += " or download <a target = \"_blank\" href = \"http://get.adobe.com/reader/\">Adobe PDF Reader</a> to view the file.";
+            embed += "</object>";
+            TempData["Embed"] = string.Format(embed, VirtualPathUtility.ToAbsolute("~/Files/Mudassar_Khan.pdf"));
+
+            return RedirectToAction("Index");
+        }
     }
 }
