62941695f9aa47f137d97b96ffa3315700d84767
Jason Clarke <jclarke@student.neumont.edu>
2020-03-04 18:16:51 -0700

    Something

--- Mysterious-Insiders/Controllers/ModularSheetController.cs
+++ Mysterious-Insiders/Controllers/ModularSheetController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Mysterious_Insiders.Services;
 
-namespace Mysterious_Insiders.Controllers
-{
-    public class ModularSheetController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+namespace Mysterious_Insiders.Controllers {
+
+public class ModularSheetController : Controller {
+
+private readonly SheetService sheetService;
+
+public ModularSheetController(SheetService service) { sheetService = service; }
+
+public IActionResult Index() {
+return View(sheetService.Get().First()); }
+
+}
+
 }
