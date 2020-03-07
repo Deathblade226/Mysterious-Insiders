c74153e249829d63f4e1b5c83d643435396794f2
Jason Clarke <jclarke@student.neumont.edu>
2020-03-06 18:40:13 -0700

    work

--- Mysterious-Insiders/Controllers/ModularSheetController.cs
+++ Mysterious-Insiders/Controllers/ModularSheetController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Mysterious_Insiders.Models;
 using Mysterious_Insiders.Services;
@@ -15,7 +16,8 @@ private readonly SheetService sheetService;
 public ModularSheetController(SheetService service) { sheetService = service; }
 
 public IActionResult Index(string id = "5e572d68083f8b4924a2411f") {
-    sheetService.CreateDnDSheet();
+    string user = HttpContext.Session.GetString("username");
+    //sheetService.CreateDnDSheet("andrew", "This is a test");
 return View(sheetService.Get(id)); }
 
 }
